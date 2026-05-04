f44756aa58993359925bf6cdb27743fa3353630b
Simone Letizi <50908915+sletizi@users.noreply.github.com>
2026-05-04 23:04:30 +0200

    gh pages config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     vue(),
     vuetify({ autoImport: true })
